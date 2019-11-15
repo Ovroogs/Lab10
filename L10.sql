2db41bba17a2cabbddef8e3c8783309d201ea593
Ovroogs <saert.tv@gmail.com>
2019-11-15 23:58:57 +0300

    added Lab10

---
+++ L10.sql
@@ -0,0 +1,14 @@
+use [g374_l10_Manin_Alekseev]
+go
+CREATE TABLE [pt](
+id SmallINT NOT NULL,
+PlanetName NVARCHAR(50),
+DistanceFromSun FLOAT,
+Imhabinats NVARCHAr(50));
+insert into [pt]VaLUES
+(1, 'Меркурий', 58875, 'Меркуриане')
+INSERT INTO [pt]VALUES
+(2, 'Венера', 85944, 'Венериане')
+INSERT INTO [pt]VALUES
+(3, 'Земля', 11895, 'Земляне')
+Select * From [pt]
---
+++ gr374_Manin_Alekseev/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace gr374_Manin_Alekseev
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'g374_l10_Manin_AlekseevDataSet.pt' table. You can move, or remove it, as needed.
+            this.ptTableAdapter.Fill(this.g374_l10_Manin_AlekseevDataSet.pt);
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ptBindingSource.EndEdit();
+            ptTableAdapter.Update(g374_l10_Manin_AlekseevDataSet);
+        }
+    }
+}
